97f586b63b327cca796fd719f53fb496afeca835
Osama Sakhi <msakhi21@gmail.com>
2019-03-31 16:55:29 -0400

    More process oriented progress

--- process_oriented/tester.py
+++ process_oriented/tester.py
@@ -1,3 +1,4 @@
 import numpy as np
 from util import *
 from vehicle import *
+from state import *
--- process_oriented/vehicle.py
+++ process_oriented/vehicle.py
@@ -19,7 +19,11 @@ LENGTHS = {
 N_LANES         = 2  # Two lane, one-way roads
 LANES_POSSIBLE  = [1, 2]
 
+
 class Vehicle():
+    """
+    A class to capture vehicle driving
+    """
 
     def __init__(self, vehicle_type):
 
@@ -27,15 +31,15 @@ class Vehicle():
             raise ValueError(f"Vehicle type '{vehicle_type}' not supported.")
 
         self.vehicle_type  = vehicle_type
-
         self.vel           = VELOCITIES[vehicle_type]
         self.length        = LENGTHS[vehicle_type]
 
-        self.lane          = 1  # Which lane vehicle is in. Can be 1 or 2.
-        self.y             = 0  # How far along the track we've gone.
+        self.turned        = False  # Indicates if this turned off Peachtree street.
+        self.lane          = 1      # Which lane vehicle is in. Can be 1 or 2.
+        self.y             = 0      # How far along the track we've gone.
 
-        self.start_time   = None
-        self.end_time     = None
+        # Track time steps taken by this vehicle.
+        self.total_time   = 0
 
 
     def drive(self, vehicle_positions, dt):
@@ -44,124 +48,35 @@ class Vehicle():
         at this time step.
         :dt: delta_t, the timestep to move by.
         """
-        v2_length = self.length
+        # Update time spent.
+        self.total_time += dt
 
-        v2_y_fullstep  = self.y + self.vel * dt          # Full speed
-        v2_y_halfstep  = self.y + (self.vel * 0.5 * dt)  # Half speed
+        v1_length = self.length
 
-        switch = switch_lane()
+        v1_y_fullstep  = self.y + self.vel * dt          # Full speed
+        v1_y_halfstep  = self.y + (self.vel * 0.5 * dt)  # Half speed
+
+        switch = switch_lane()  # Switch lane with some probability
         if switch:
-            v2_lane = 2 if self.lane == 1 else 1
+            v1_lane = 2 if self.lane == 1 else 1  # Flip the lane
         else:
-            v2_lane = self.lane
+            v1_lane = self.lane
 
-        fullstep_valid   = True
-        halfstep_valid   = True
+        if can_place(vehicle_positions, v1_lane, v1_y_fullstep, v1_length):
+            return (v1_lane, v1_y_fullstep, v1_length)
 
-        # if can_place(vehicle_positions, v1_lane, v1_y, v1_length)
+        elif can_place(vehicle_positions, v1_lane, v1_y_halfstep, v1_length):
+            return (v1_lane, v1_y_halfstep, v1_length)
 
-        # Check if either half step or full step is fine with this v2_lane.
-        # Start evaluating full step. If this creates a collision
-        # we will switch to half step. Otherwise we won't move.
-        for (v1_lane, v1_y, v1_length) in vehicle_positions:
+        elif switch and can_place(vehicle_positions, self.lane, v1_y_fullstep, v1_length):
+            return (v1_lane, v1_y_fullstep, v1_length)
 
-            # Try going at full velocity.
-            if fullstep_valid:
-                if will_collide(v1_lane, v1_y, v1_length, v2_lane, v2_y_fullstep,
-                                v2_length):
-                    fullstep_valid = False
-                else:
-                    continue  # Continue if no problem so far
+        elif switch and can_place(vehicle_positions, self.lane, v1_y_halfstep, v1_length):
+            return (v1_lane, v1_y_halfstep, v1_length)
 
-            # Try going at half velocity.
-            if halfstep_valid:
-                if will_collide(v1_lane, v1_y, v1_length, v2_lane, v2_y_halfstep,
-                                v2_length):
-                    halfstep_valid = False
-                    break  # Break out of the loop
-                else:
-                    continue  # Continue if no problem so far
+        return (self.lane, self.y, self.length)
 
 
-        if fullstep_valid:
-            return (v2_lane, v2_y_fullstep, v2_length)
-        elif halfstep_valid:
-            return (v2_lane, v2_y_halfstep, v2_length)
-        elif not switch:
-            # If we never switched lanes, we must just stay put in the same spot.
-            return (self.lane, self.y, v2_length)
-
-
-        # If we simulated lane change, undo lane change
-        v2_lane         = self.lane
-        fullstep_valid  = True
-        halfstep_valid  = True
-
-        # Repeat above step, but now staying in same lane.
-        for (v1_lane, v1_y, v1_length) in vehicle_positions:
-
-            # Try going at full velocity.
-            if fullstep_valid:
-                if will_collide(v1_lane, v1_y, v1_length, v2_lane, v2_y_fullstep,
-                                v2_length):
-                    fullstep_valid = False
-                else:
-                    continue  # Continue if no problem so far
-
-            # Try going at half velocity.
-            if halfstep_valid:
-                if will_collide(v1_lane, v1_y, v1_length, v2_lane, v2_y_halfstep,
-                                v2_length):
-                    halfstep_valid = False
-                    break  # Break out of the loop
-                else:
-                    continue  # Continue if no problem so far
-
-
-        if fullstep_valid:
-            return (v2_lane, v2_y_fullstep, v2_length)
-        elif halfstep_valid:
-            return (v2_lane, v2_y_halfstep, v2_length)
-
-        # We must just stay put in the same spot.
-        return (self.lane, self.y, v2_length)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            if not will_collide(v1_lane, v1_y, v1_length, v2_lane,
-                v2_y, v2_length):
-                continue  # Continue if no problem so far
-
-            # If we collide:
-            if v2_y == v2_y_fullstep:
-                v2_y
-
-            # Try going at half velocity.
-            elif not will_collide(v1_lane, v1_y, v1_length, v2_lane,
-                v2_y_halfstep, v2_length):
-                continue  # Continue if no problem so far
-
-            # Try going at half velocity.
-            elif not will_collide(v1_lane, v1_y, v1_length, v2_lane,
-                v2_y_halfstep, v2_length):
-                continue  # Continue if no problem so far
+    def done_driving(self, y_max):
+        return (self.turned or self.y >= y_max)
 
